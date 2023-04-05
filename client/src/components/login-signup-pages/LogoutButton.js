ac9249051ec8b7818b61d8108549fd744d16fb52
1MayoBrahim <mayobrahim@hotmail.com>
2023-04-05 12:28:58 -0400

    Create LogoutButton component

---
+++ client/src/components/login-signup-pages/LogoutButton.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { CurrentUseContext } from "../all-contexts/currentUserContext";
+import { FiLogout } from "react-icons/fi";
+import { deleteLoginSession } from "../helpers/express-session-helpers";
+
+// A log out button in the current use profile. it will calls the endpoint
+// that clears all currentUser data info in the database collection
+// "currentUserContext"
+
+const LogoutButton = () => {
+  const navigate = useNavigate();
+  const { setIsUserLoggedIn, setCurrentUser } = useContext(CurrentUseContext);
+
+  const handleLogout = () => {
+    deleteLoginSession();
+    setIsUserLoggedIn(false);
+    setCurrentUser(null);
+    navigate("/");
+  };
+  return (
+    <Button onClick={() => handleLogout()}>
+      <span>FiLogout</span> <FiLogout />
+    </Button>
+  );
+};
+
+const Button = styled.div`
+  padding: 5px 20px;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ee6c4d;
+  color: white;
+  border: 2px solid white;
+  border-radius: 6px;
+  cursor: pointer;
+  span {
+    margin-right: 4px;
+  }
+  @media (max-height: 736px) {
+    font-size: 0.75em;
+  }
+`;
+
+export default LogoutButton;
